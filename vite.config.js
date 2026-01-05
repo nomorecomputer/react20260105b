eb093f3cbfcdf02e83733b45c970a6ba6a94a31a
WeiLin <nomorecomputer@gmail.com>
2026-01-05 17:01:49 +0800

    add gh-pages、adjust base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import process from "process";
 
-// https://vite.dev/config/
 export default defineConfig({
   server: { open: true },
   plugins: [react()],
+  base: process.env.NODE_ENV === "process" ? "/reaction20260105b/" : "/",
 });
